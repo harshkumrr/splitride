ce7a2a759beada835f3e215865d96cd19d96768e
Harsh Kumar <harshkumar89412@gmail.com>
2026-08-08 18:08:21 +0530

    Initial Spring Boot project setup with MySQL connection

---
+++ src/main/java/com/splitride/splitride/SplitrideApplication.java
@@ -0,0 +1,13 @@
+package com.splitride.splitride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitrideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitrideApplication.class, args);
+	}
+
+}
